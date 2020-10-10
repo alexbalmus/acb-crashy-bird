5546e667e21ee4d9bed7ebb34953f21d5e9d468a
Alexandru-Cristian Balmus <alexbalmus@gmail.com>
2020-10-10 16:06:19 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
-input.onButtonPressed(Button.A, function () {
+function btnAHandler() 
+{
     bird.change(LedSpriteProperty.Y, -1);
-});
+}
 
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.A, btnAHandler);
+
+function btnBHandler() 
+{
     bird.change(LedSpriteProperty.Y, 1);
-});
+}
+
+input.onButtonPressed(Button.B, btnBHandler);
 
 let emptyObstacleY: number = 0;
 let ticks: number = 0;
@@ -15,32 +21,43 @@ let obstacles: game.LedSprite[] = [];
 bird = game.createSprite(0, 2);
 bird.set(LedSpriteProperty.Blink, 300);
 
-basic.forever(function () {
-    while (obstacles.length > 0 && obstacles[0].get(LedSpriteProperty.X) == 0) {
+function main() 
+{
+    while (obstacles.length > 0 && 
+        obstacles[0].get(LedSpriteProperty.X) == 0) 
+    {
         obstacles.removeAt(0).delete();
     }
 
-    for (let obstacle2 of obstacles) {
+    for (let obstacle2 of obstacles) 
+    {
         obstacle2.change(LedSpriteProperty.X, -1);
     }
 
-    if (ticks % 3 == 0) {
+    if (ticks % 3 == 0) 
+    {
         emptyObstacleY = randint(0, 4);
 
-        for (let index2 = 0; index2 <= 4; index2++) {
-            if (index2 != emptyObstacleY) {
+        for (let index2 = 0; index2 <= 4; index2++) 
+        {
+            if (index2 != emptyObstacleY) 
+            {
                 obstacles.push(game.createSprite(4, index2));
             }
         }
     }
 
-    for (let obstacle3 of obstacles) {
+    for (let obstacle3 of obstacles) 
+    {
         if (obstacle3.get(LedSpriteProperty.X) == bird.get(LedSpriteProperty.X) &&
-         obstacle3.get(LedSpriteProperty.Y) == bird.get(LedSpriteProperty.Y)) {
+            obstacle3.get(LedSpriteProperty.Y) == bird.get(LedSpriteProperty.Y)) 
+        {
             game.gameOver();
         }
     }
 
     ticks += 1;
     basic.pause(1000);
-});
+}
+
+basic.forever(main);
